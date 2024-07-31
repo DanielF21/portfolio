c0270a31059cb449e3b0f0adc4ffd186f347103f
Daniel Fleming <dfleming@mit.edu>
2024-07-30 22:53:10 -0400

    Updated portfolio assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,6 +152,7 @@ export default function Page() {
           </div>
         </div>
       </section>
+      {/*
       <section id="hackathons">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 13}>
@@ -163,13 +164,16 @@ export default function Page() {
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                   I like building things
                 </h2>
+                
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                  
                   During my time in university, I attended{" "}
                   {DATA.hackathons.length}+ hackathons. People from around the
                   country would come together and build incredible things in 2-3
                   days. It was eye-opening to see the endless possibilities
                   brought to life by a group of motivated and passionate
                   individuals.
+                  
                 </p>
               </div>
             </div>
@@ -195,6 +199,7 @@ export default function Page() {
           </BlurFade>
         </div>
       </section>
+      */}
       <section id="contact">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
@@ -213,8 +218,7 @@ export default function Page() {
                 >
                   with a direct question on twitter
                 </Link>{" "}
-                and I&apos;ll respond whenever I can. I will ignore all
-                soliciting.
+                and I&apos;ll respond whenever I can.
               </p>
             </div>
           </BlurFade>
