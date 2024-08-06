1dacd87bb97ac2b83243a51a2a4410ff7e267331
Daniel Fleming <dfleming@mit.edu>
2024-08-06 09:01:34 -0400

    moved backend to render

--- next.config.mjs
+++ next.config.mjs
@@ -5,40 +5,9 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/:path*', // Proxy to Backend
-      }
-    ]
-  }
-}
+        destination: 'https://scheme-interpreter.onrender.com/:path*',
+      },
+    ];
+  },
+};
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   rewrites: async () => {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         destination:
-//           process.env.NODE_ENV === "development"
-//             ? "http://127.0.0.1:8000/api/:path*"
-//             : "/api/",
-//       },
-//       // {
-//       //   source: "/docs",
-//       //   destination:
-//       //     process.env.NODE_ENV === "development"
-//       //       ? "http://127.0.0.1:8000/docs"
-//       //       : "/api/docs",
-//       // },
-//       // {
-//       //   source: "/openapi.json",
-//       //   destination:
-//       //     process.env.NODE_ENV === "development"
-//       //       ? "http://127.0.0.1:8000/openapi.json"
-//       //       : "/api/openapi.json",
-//       // },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
